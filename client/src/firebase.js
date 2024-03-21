712c9d48b3b39b0285a55c31c4114ffcc7caec87
Sreenath <sreenathsrp256@gmail.com>
2024-03-21 21:33:59 +0530

    First

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBEVcrlNu8Z6ukW2x8l3T615lSMJbgx7qA",
+  authDomain: "codedrops-cfbc2.firebaseapp.com",
+  projectId: "codedrops-cfbc2",
+  storageBucket: "codedrops-cfbc2.appspot.com",
+  messagingSenderId: "400449465783",
+  appId: "1:400449465783:web:d7bf076fea500d8d79cc32"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
